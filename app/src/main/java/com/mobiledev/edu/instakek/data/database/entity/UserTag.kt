8c075a089e0387754bf9767160d55494c3854ad9
Anna Tsytsyliuk <ann.tsytsyluik@gmail.com>
2019-06-17 13:28:03 +0300

    Add userTag

---
+++ app/src/main/java/com/mobiledev/edu/instakek/data/database/entity/UserTag.kt
@@ -0,0 +1,43 @@
+package com.mobiledev.edu.instakek.data.database.entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+
+
+@Entity(
+        tableName = "user_tag",
+        foreignKeys = arrayOf(
+                ForeignKey(
+                        entity = Tag::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("tag_id"),
+                        onDelete = ForeignKey.NO_ACTION
+                ),
+                ForeignKey(
+                        entity = User::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("user_id"),
+                        onDelete = ForeignKey.NO_ACTION
+                ),
+                ForeignKey(
+                        entity = UserTagType::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("tag_type_id"),
+                        onDelete = ForeignKey.NO_ACTION
+                )
+        )
+)
+class UserTag (
+    @ColumnInfo(name = "user_id")
+    @PrimaryKey(autoGenerate = false)
+    var userId: Long,
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "tag_id")
+    var tagId: Long,
+    @ColumnInfo(name = "tag_type_id")
+    var tagTypeId: Long
+
+)
+{}
---
+++ app/src/main/java/com/mobiledev/edu/instakek/data/database/entity/UserTagType.kt
@@ -0,0 +1,16 @@
+package com.mobiledev.edu.instakek.data.database.entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+
+@Entity(
+        tableName = "user_tag_type"
+)
+class UserTagType (
+    @PrimaryKey(autoGenerate = true)
+    var id: Long?,
+    @ColumnInfo(name = "tag_type_name")
+    var tagTypeName: String
+)
