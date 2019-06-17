b46eb6b76e89e7c8ea7e857b27f927c1cf10df3b
Anna Tsytsyliuk <ann.tsytsyluik@gmail.com>
2019-06-17 13:13:30 +0300

    Add likes + postContent

---
+++ app/src/main/java/com/mobiledev/edu/instakek/data/database/entity/Likes.kt
@@ -0,0 +1,33 @@
+package com.mobiledev.edu.instakek.data.database.entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(
+        tableName = "likes",
+        foreignKeys = arrayOf(
+                ForeignKey(
+                        entity = Post::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("post_id"),
+                        onDelete = ForeignKey.NO_ACTION
+                ),
+                ForeignKey(
+                        entity = User::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("user_id"),
+                        onDelete = ForeignKey.NO_ACTION
+                )
+        )
+)
+class Likes (
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "user_id")
+    var userId: Long?,
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "post_id")
+    var postId: Long
+)
+{}
---
+++ app/src/main/java/com/mobiledev/edu/instakek/data/database/entity/PostContent.kt
@@ -0,0 +1,28 @@
+package com.mobiledev.edu.instakek.data.database.entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+
+
+@Entity(
+        tableName = "post_content",
+        foreignKeys = arrayOf(
+                ForeignKey(
+                        entity = Post::class,
+                        parentColumns = arrayOf("id"),
+                        childColumns = arrayOf("post_id"),
+                        onDelete = ForeignKey.NO_ACTION
+                )
+        )
+)
+class PostContent (
+    @PrimaryKey(autoGenerate = true)
+    var id: Long?,
+    @ColumnInfo(name = "post_id")
+    var postId: Long,
+    @ColumnInfo(name = "content_link")
+    var contentLink: String
+)
+{}
